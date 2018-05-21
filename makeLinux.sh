f39395e719761db286531db3290b940fe718e5cb
xlrth <roax@gmx.de>
2018-05-21 21:46:54 +0200

    typos

--- makeLinux.sh
+++ makeLinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-c++ -o backup -flto -O3 src/Main.cpp src/Backup.cpp src/BackupConfig.cpp src/Helpers.cpp src/SqliteWrapper.cpp stc/sqlite3.c -lstdc++fs
+c++ -o backup -flto -O3 src/Main.cpp src/Backup.cpp src/BackupConfig.cpp src/Helpers.cpp src/SqliteWrapper.cpp src/sqlite3.c -lstdc++fs
