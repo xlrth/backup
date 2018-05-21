3fdfda0f88d1d9df7448122cd0f653082aa97694
xlrth <roax@gmx.de>
2018-05-21 20:55:23 +0200

    adapted linux make script

--- makeLinux.sh
+++ makeLinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-c++ -o backup -flto -O3 src/backupConfig.cpp src/helpers.cpp src/backup.cpp src/sqliteWrapper.cpp -lsqlite3 -lstdc++fs
+c++ -o backup -flto -O3 src/Backup.cpp src/BackupConfig.cpp src/Helpers.cpp src/SqliteWrapper.cpp stc/sqlite3.c -lstdc++fs
